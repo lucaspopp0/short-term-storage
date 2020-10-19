6d02bc5882057a30ef0076ceb925b668557047ba
Lucas Popp <lmp122@case.edu>
2020-10-19 11:57:49 -0400

    Print any objects in folder older than timeout

--- sts.py
+++ sts.py
@@ -1,4 +1,6 @@
 import argparse
+import os
+import datetime
 
 parser = argparse.ArgumentParser(description='Set up STS on a folder')
 
@@ -6,3 +8,15 @@ parser.add_argument('-f', '--folder', required=True, help='The folder to set up 
 parser.add_argument('-t', '--timeout', required=True, type=int, help='The max time a file can be in the folder for')
 
 args = parser.parse_args()
+
+contents = os.listdir(args.folder)
+now = datetime.datetime.now()
+
+for obj in contents:
+    absPath = os.path.join(args.folder, obj)
+    atime = os.path.getatime(absPath)
+    parsed = datetime.datetime.fromtimestamp(atime)
+    diff = now - parsed
+    total = diff.total_seconds()
+    if total > args.timeout:
+        print(obj)
